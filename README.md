a3e4c5441ef8aa220bdb1b835459c5292f7ab45a
Narendran Ravikumar <narendran89.ravi@gmail.com>
2023-03-08 14:12:56 +0000

    Adding python script for generating certificates

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ This python script simplifies the generation of self-signed certificates that we
 
 The script expects that you have already generated your CA certificate and is placed in the same directory as that of main.py <br>
 The name of the CA key to be ca.key <br>
-The name of the CA cert to be cacert.crt
+The name of the CA cert to be cacert.crt <br> <br>
+
+Update the server.cnf with the default values for Company, department, Country etc., except for Common Name. A placeholder - <COMMON_NAME> is added for the script to find and replace depending upon the provided value. <br>
+**NOTE:** Once the script is executed, it will take the default values provided in the server.cnf
+
+The script should also be executed in an environment that has openssl installed.
 
 ### Using the python script
 
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import fileinput
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_cert(cert_domain):
+    os.mkdir(os.getcwd() + "/" + cert_domain)
+    replace_text_file("server.cnf", "<COMMON_NAME>", cert_domain)
+    replace_text_file("server.ext", "<COMMON_NAME>", cert_domain)
+    generate_server_certs()
+    move_generated_files()
 
 
-# Press the green button in the gutter to run the script.
+def replace_text_file(file, text_to_find, text_to_replace):
+    with fileinput.FileInput(file, inplace=True) as file:
+        for line in file:
+            print(line.replace(text_to_find, text_to_replace), end='')
+
+
+def generate_server_certs():
+    os.system("openssl genrsa -out server.key")
+    os.system("openssl req -config server.cnf -new -batch -key server.key -out server.csr")
+    os.system(
+        "openssl x509 -req -in server.csr  -CA cacert.crt -CAkey ca.key -out server.crt  -CAcreateserial -CAserial serial -days 365 -sha512 -extfile server.ext")
+
+
+def move_generated_files():
+    os.replace("server.crt", os.getcwd() + "/" + cert_domain + "/server.crt")
+    os.replace("server.csr", os.getcwd() + "/" + cert_domain + "/server.csr")
+    os.replace("server.key", os.getcwd() + "/" + cert_domain + "/server.key")
+    os.replace("serial", os.getcwd() + "/" + cert_domain + "/serial")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cert_domain = input("Enter the Common name for the certificate : ")
+    create_cert(cert_domain)
+    replace_text_file("server.cnf", cert_domain, "<COMMON_NAME>")
+    replace_text_file("server.ext", cert_domain, "<COMMON_NAME>")
